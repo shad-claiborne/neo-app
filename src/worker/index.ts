8b1401013c38eef452cfecb3ffc8bac0bdca7bba
Shad Claiborne <shad.claiborne@outlook.com>
2026-03-01 04:05:30 -0800

    integerated oidc

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,19 @@
 import { Hono } from "hono";
+import { except } from "hono/combine";
+import { forAuthorization, withIdentity } from "@shad-claiborne/hono-middleware-oidc";
+
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.use('*', except(['/login', '/oauth/callback'], withIdentity));
+
+app.use('/oauth/callback', forAuthorization);
+
+app.get('/login', async (c, next) => {
+    const appUrl = new URL('/', c.req.url);
+    c.set('originUrl', appUrl.toString());
+    return await withIdentity(c, next);
+});
+
+app.get('/id', async (c) => c.json(c.get('identity')));
 
 export default app;
